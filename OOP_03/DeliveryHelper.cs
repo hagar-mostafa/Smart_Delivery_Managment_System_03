0041ff0ec57823257079ead8f44438f423881a12
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-20 09:13:04 +0200

    DeliveryHelper Class

---
+++ OOP_03/DeliveryHelper.cs
@@ -0,0 +1,16 @@
+﻿using OOP_03.Shipments;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_03
+{
+    static public class DeliveryHelper
+    {
+       static public void PrintShipmentDetails(Shipment Shipments)
+        {
+
+            Shipments.PrintShipment();
+        }
+    }
+}
