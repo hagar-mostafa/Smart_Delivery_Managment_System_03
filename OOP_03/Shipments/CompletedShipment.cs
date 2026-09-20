613dd9e41fc15ccac8cf584fcc0f99b5af3008e5
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-20 09:18:48 +0200

    CompletedShipment Class

---
+++ OOP_03/Shipments/CompletedShipment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_03.Shipments
+{
+    public sealed class CompletedShipment  : Shipment
+    {
+        // we must make Chaining Constructor to prevent the error 
+        // and that means we overload on parameterless constructor 
+        // i know that every class has implict parameterless constructor
+        public CompletedShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination) : base(trackingCode, description, weight, deliveryFee, destination) { }
+    }
+}
